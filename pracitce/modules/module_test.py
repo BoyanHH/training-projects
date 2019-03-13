73114020b0fccd5ab9fd6a3d8c097931fca7ae34
root <root@192.168.1.123>
2019-03-13 17:47:10 +0200

    python backup v2

---
+++ pracitce/modules/module_test.py
@@ -0,0 +1,4 @@
+#module - the highest level program organization unit(A Python file).
+#import - Lets a client fetch a module as a whole
+#from  - Allows clients to fetch particular names from a module
+#imp.reload - reload a module's code without stopping Python
